364c37b2c53dfb80465663c18c15a78c3e56b44e
Lincoln <Lincoln@usa.com>
2022-04-07 14:07:58 +0800

    增加 ndarray 的操作

---
+++ NP/nputils.py
@@ -0,0 +1,8 @@
+def ndarrayInfo(nda):
+	# 看看 ndarray 的基础信息，当然，这需要你的基础知识的掌握
+	print(" ndim:    ", nda.ndim)
+	print(" shape:   ", nda.shape)
+	print(" size:    ", nda.size)
+	print(" dtype:   ", nda.dtype)
+	print(" itemsize:", nda.itemsize, " bytes")
+	print(" nbytes:  ", nda.nbytes, " bytes")
--- README.md
+++ README.md
@@ -7,3 +7,6 @@ Python当中的使用的一些模块、类和方法
 ## 项目代码结构
 
 1. ByteDance 字节体系的项目的文件
+2. NP 关于 numpy 的一些操作
+3. VideoAudio 关于音频视频的一些操作
+4. TK-Plots 一个使用 TK 做的公式图打印的程序
